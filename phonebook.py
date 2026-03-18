9c4305fa427f063750573c29754579ed7156c00b
Toros-Manuk <toros.manuk21@gmail.com>
2026-03-18 14:51:57 +0200

    created first functions of the phonebook: "delete", "search", "show"

--- phonebook.py
+++ phonebook.py
@@ -1,6 +1,5 @@
-phonebook = [
 
-]
+phonebook = []
 
 while True:
     command = input("Enter command (add/delete/search/show/exit):")
@@ -10,3 +9,17 @@ while True:
         name = input("Enter name:")
         phone = input("Enter phone number:")
         phonebook.append({"name":name, "phone":phone})
+    elif command == "delete":
+        id = int(input("Enter the ID of the user to delete"))
+        phonebook.pop(id)
+    elif command == "search":
+        name = input("Enter the name of the man of your dream")
+        for person in phonebook:
+            if person["name"] == name:
+                print(f'User {person["name"]} - {person["phone"]}')
+                break
+        else:
+            print("Person with such name is not found")
+    elif command == "show":
+        for i in range(len(phonebook)):
+            print(phonebook[i]["name"], phonebook[i]["phone"])
